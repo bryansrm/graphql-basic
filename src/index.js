1ec68b642c65697e654f15b54fd36af19546f940
Bryan <bsteven1993@gmail.com>
2019-04-04 23:26:22 -0500

    config graphql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Ejemplo con Graphql");
+});
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}));
 
 app.listen(3000, () => console.log('Server, on port 3000'));
